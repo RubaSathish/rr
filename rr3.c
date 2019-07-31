79fbc72984ee1f96f6839064254fd64dfdb3fb51
RubaSathish <53251305+RubaSathish@users.noreply.github.com>
2019-07-31 15:38:51 +0500

    Create rr3.c

---
+++ rr3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char a[100];
+    int i,c=0;
+    scanf("%[^\n]",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]!=' ')
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+return 0;
+}
